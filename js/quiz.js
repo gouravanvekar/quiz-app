b11e89adbeb69ee4b8f2b789ccfbee1f1bbd9014
Gourav Anvekar <gourav.anvekar@gmail.com>
2015-09-10 18:00:59 -0400

    Populated json and configured progress bar

--- js/quiz.js
+++ js/quiz.js
@@ -3,9 +3,15 @@
 	var app = angular.module('myQuiz', []);
 
     app.controller('QuizController', ['$scope', '$http', '$sce', function($scope, $http, $sce){
+        $scope.score = 0;
+        $scope.activeQuestion = -1;
+        $scope.activeQuestionAnswered = 0;
+        $scope.percentage = 0;
 
-
-
+        $http.get('quiz_data.json').then(function(quizData){
+            $scope.myQuestions = quizData.data;
+            $scope.totalQuestions = $scope.myQuestions.length;
+        });
     }]);
 
 })();
